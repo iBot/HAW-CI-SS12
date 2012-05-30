052efb1c70eb12657adf8840c1f8d897a60465d7
iBot <>
2012-05-30 11:38:26 +0200

    Methode zum erstellen von Labels zu CodeGenerator hinzugefügt

--- HAW-CI-SS12/src/prak2/CodeGenerator.java
+++ HAW-CI-SS12/src/prak2/CodeGenerator.java
@@ -8,13 +8,8 @@ import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import prak3.descr.AbstractDescr;
-import prak3.node.AbstractNode;
-import prak3.node.IndexExpressionNode;
 import prak3.node.Node;
-import prak3.node.SimpleExpressionNode;
 
 /**
  *
@@ -27,18 +22,31 @@ public class CodeGenerator {
     public static String targetFile = "assembler.txt";
     private static FileWriter fstream;
     private static BufferedWriter out;
+    private static int addressCounter;
+    private static int labelId;
     
     public static void generateCode(Node startnode){
+        CodeGenerator.labelId = 0;
+        CodeGenerator.addressCounter = 0;
         CodeGenerator.startnode = startnode;
         symboltable = new HashMap<String, AbstractDescr>();
         try {
             fstream = new FileWriter(targetFile);
             out = new BufferedWriter(fstream);
-            ((IndexExpressionNode)startnode).compile(symboltable);
+            startnode.compile(symboltable);
             out.close();
         } catch (IOException ex) {
             System.err.println("Error: " + ex.getMessage());
         }
+    }
+    
+    public static int getCurrentAddress(){
+        return addressCounter;
+    }
+    
+    public static int increaseAddressBy(int size){
+        addressCounter+=size;
+        return addressCounter;
     }
     
     public static void writeln(String codeline){
@@ -50,5 +58,8 @@ public class CodeGenerator {
         }
     }
     
+    public static String getLabel(){
+        return String.format("LBL%d", labelId++);
+    }
     
 }
